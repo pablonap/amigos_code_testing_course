2d6e2f2f2047d2b306fd2be58d76a4817c156dec
chipo <pnapoli33@gmail.com>
2021-03-05 00:03:27 -0300

    Initial commit

---
+++ src/main/java/amigos_code_prj01/AmigosCodePrj01Application.java
@@ -0,0 +1,13 @@
+package amigos_code_prj01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigosCodePrj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigosCodePrj01Application.class, args);
+	}
+
+}
---
+++ src/test/java/amigos_code_prj01/AmigosCodePrj01ApplicationTests.java
@@ -0,0 +1,13 @@
+package amigos_code_prj01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigosCodePrj01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

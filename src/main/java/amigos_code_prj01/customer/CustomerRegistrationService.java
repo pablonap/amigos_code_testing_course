13c6f51a22b4d6f4e69d6195b3c5757e955ed6e8
chipo <pnapoli33@gmail.com>
2021-03-06 17:25:20 -0300

    Add logic for registerNewCustomer method from CustomerRegistrationService

--- src/main/java/amigos_code_prj01/customer/CustomerRegistrationService.java
+++ src/main/java/amigos_code_prj01/customer/CustomerRegistrationService.java
@@ -1,5 +1,7 @@
 package amigos_code_prj01.customer;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,16 +10,26 @@ public class CustomerRegistrationService {
 	
 	private final CustomerRepository customerRepository;
 	
-	
 	@Autowired
 	public CustomerRegistrationService(CustomerRepository customerRepository) {
 		this.customerRepository = customerRepository;
 	}
 
-
-
 	public void registerNewCustomer(CustomerRegistrationRequest request) {
-		 
+		Customer requestedCustomer = request.getCustomer();
+		
+		Optional<Customer> optCustomerFromBd = customerRepository.selectCustomerByPhoneNumber(requestedCustomer.getPhoneNumber());
+
+		if (optCustomerFromBd.isPresent()) {
+			Customer customerFromBd = optCustomerFromBd.get();
+			if (customerFromBd.getName().equals(requestedCustomer.getName())) {
+				return;
+			} 
+			
+			throw new IllegalStateException(String.format("phone number [%s] is taken", requestedCustomer.getPhoneNumber()));
+		}
+		
+		customerRepository.save(requestedCustomer);
 	}
 
 }

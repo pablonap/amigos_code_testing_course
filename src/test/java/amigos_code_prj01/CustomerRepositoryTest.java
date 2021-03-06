c3974cbaa703ea310f390c21ed2bdbd681275c1f
chipo <pnapoli33@gmail.com>
2021-03-06 15:35:44 -0300

    Add test case for itShouldSaveCustomer

--- src/test/java/amigos_code_prj01/CustomerRepositoryTest.java
+++ src/test/java/amigos_code_prj01/CustomerRepositoryTest.java
@@ -1,11 +1,16 @@
 package amigos_code_prj01;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
+import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import amigos_code_prj01.customer.Customer;
 import amigos_code_prj01.customer.CustomerRepository;
 
 @DataJpaTest
@@ -21,8 +26,27 @@ class CustomerRepositoryTest {
 		// when
 		
 		// then
-		assertTrue(true);
-		 
+	}
+
+	@Test
+	void itShouldSaveCustomer() {
+		// given
+		UUID id = UUID.randomUUID();
+		Customer customer = new Customer(id, "Abel", "0000");
+		
+		// when
+		underTest.save(customer);
+		
+		// then
+		Optional<Customer> optionalCustomer = underTest.findById(id);
+		assertThat(optionalCustomer)
+		.isPresent()
+		.hasValueSatisfying(c -> {
+			assertThat(c.getId()).isEqualTo(id);
+			assertThat(c.getName()).isEqualTo("Abel");
+			assertThat(c.getPhoneNumber()).isEqualTo("0000");
+		});
+
 	}
 
 }

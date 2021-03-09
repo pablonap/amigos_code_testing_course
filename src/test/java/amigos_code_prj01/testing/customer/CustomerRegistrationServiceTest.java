9eaab1e9ccf05489c86c7ee3a5c0f58790d1ca05
chipo <pnapoli33@gmail.com>
2021-03-08 23:08:48 -0300

    Add "itShouldSaveNewCustomer" test case in CustomerRegistrationServiceTest

--- src/test/java/amigos_code_prj01/testing/customer/CustomerRegistrationServiceTest.java
+++ src/test/java/amigos_code_prj01/testing/customer/CustomerRegistrationServiceTest.java
@@ -1,21 +1,56 @@
 package amigos_code_prj01.testing.customer;
 
-import static org.mockito.Mockito.mock;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
-import org.junit.jupiter.api.BeforeEach;
+import java.util.Optional;
+import java.util.UUID;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import amigos_code_prj01.customer.Customer;
+import amigos_code_prj01.customer.CustomerRegistrationRequest;
 import amigos_code_prj01.customer.CustomerRegistrationService;
 import amigos_code_prj01.customer.CustomerRepository;
 
+@ExtendWith(MockitoExtension.class)
 public class CustomerRegistrationServiceTest {
 	
-	private CustomerRepository customerRepository = mock(CustomerRepository.class);
-
-	private CustomerRegistrationService underTest;
+	@Mock
+    private CustomerRepository customerRepository;
 	
-	@BeforeEach
-	void setUp() {
-		underTest = new CustomerRegistrationService(customerRepository);
+	@InjectMocks
+	private CustomerRegistrationService underTest;
+
+	@Captor
+	private ArgumentCaptor<Customer> customerArgumentCaptor;
+	
+	@Test
+	void itShouldSaveNewCustomer() {
+		// given
+		String phoneNumber = "777";
+		Customer customer = new Customer(UUID.randomUUID(), "luca", phoneNumber);
+		
+		CustomerRegistrationRequest request = new CustomerRegistrationRequest(customer);
+		
+		given(customerRepository.selectCustomerByPhoneNumber(phoneNumber))
+			.willReturn(Optional.empty());
+		
+		// when
+		underTest.registerNewCustomer(request);
+		
+		// then
+		then(customerRepository).should().save(customerArgumentCaptor.capture());
+		
+		Customer customerArgumentCaptorValue = customerArgumentCaptor.getValue();
+		assertThat(customerArgumentCaptorValue).isEqualTo(customer);
 	}
 
 }

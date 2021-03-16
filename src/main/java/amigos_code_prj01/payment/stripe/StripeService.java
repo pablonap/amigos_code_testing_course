962bab123de03f28fcfff6baf725e34d5e32e818
chipo <pnapoli33@gmail.com>
2021-03-15 23:01:05 -0300

    Implement StripeService

---
+++ src/main/java/amigos_code_prj01/payment/stripe/StripeService.java
@@ -0,0 +1,43 @@
+package amigos_code_prj01.payment.stripe;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+import com.stripe.net.RequestOptions;
+
+import amigos_code_prj01.payment.CardPaymentCharge;
+import amigos_code_prj01.payment.CardPaymentCharger;
+import amigos_code_prj01.payment.Currency;
+
+@Service
+//This code is taken from https://stripe.com/docs/api
+public class StripeService implements CardPaymentCharger {
+	
+	// Grab from Authentication section
+	private final static RequestOptions requestOptions = RequestOptions.builder()
+			  .setApiKey("sk_test_4eC39HqLyjWDarjtT1zdp7dc")
+			  .build();
+
+	@Override
+	// Taken from Create charge
+	public CardPaymentCharge chargeCard(String cardSource, BigDecimal amount, Currency currency, String description) {
+		Map<String, Object> params = new HashMap<>();
+		params.put("amount", amount);
+		params.put("currency", currency);
+		params.put("source", cardSource);
+		params.put("description", description);
+
+		try {
+			Charge charge = Charge.create(params, requestOptions);
+			return new CardPaymentCharge(charge.getPaid());
+		} catch (StripeException e) {
+			throw new IllegalStateException("Cannot make stripe charge", e);
+		}
+	}
+
+}

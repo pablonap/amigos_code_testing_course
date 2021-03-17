ba64b6fa3af2c846339bfb7e0bbb414bcfcaab79
chipo <pnapoli33@gmail.com>
2021-03-16 22:54:16 -0300

    Add PaymentIntegrationTest class with @SpringBootTest annotation so whenever I run a test in this class Spring will start the application.

---
+++ src/test/java/amigos_code_prj01/testing/payment/stripe/PaymentIntegrationTest.java
@@ -0,0 +1,18 @@
+package amigos_code_prj01.testing.payment.stripe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PaymentIntegrationTest {
+
+	@Test
+	void itShouldCreatePaymentSuccessfully() {
+		// given
+		
+		// when
+		
+		// then
+
+	}
+}

685462bdde88f2e50a17ad057c518af6276a4d89
chipo <pnapoli33@gmail.com>
2021-03-17 00:31:19 -0300

    Complete the "when" block from itShouldCreatePaymentSuccessfully() test case by invoking the endpoint to register a new payment

--- src/main/java/amigos_code_prj01/payment/PaymentController.java
+++ src/main/java/amigos_code_prj01/payment/PaymentController.java
@@ -1,9 +1,6 @@
 package amigos_code_prj01.payment;
 
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +16,8 @@ public class PaymentController {
         this.paymentService = paymentService;
     }
 
-    @RequestMapping("/{customerId}")
-    public void makePayment(@PathVariable("customerId") UUID customerId, @RequestBody PaymentRequest paymentRequest) {
-        paymentService.chargeCard(customerId, paymentRequest);
+    @RequestMapping
+    public void makePayment(@RequestBody PaymentRequest paymentRequest) {
+        paymentService.chargeCard(paymentRequest.getPayment().getCustomerId(), paymentRequest);
     }
 }
--- src/test/java/amigos_code_prj01/testing/payment/stripe/PaymentIntegrationTest.java
+++ src/test/java/amigos_code_prj01/testing/payment/stripe/PaymentIntegrationTest.java
@@ -1,7 +1,11 @@
 package amigos_code_prj01.testing.payment.stripe;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.math.BigDecimal;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -19,12 +23,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import amigos_code_prj01.customer.Customer;
 import amigos_code_prj01.customer.CustomerRegistrationRequest;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import amigos_code_prj01.payment.Currency;
+import amigos_code_prj01.payment.Payment;
+import amigos_code_prj01.payment.PaymentRepository;
+import amigos_code_prj01.payment.PaymentRequest;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class PaymentIntegrationTest {
+	
+	@Autowired
+	private PaymentRepository paymentRepository;
 	
     @Autowired
     private MockMvc mockMvc;
@@ -39,11 +48,33 @@ public class PaymentIntegrationTest {
 		ResultActions customerRegResultActions = mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/customer-registration")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(Objects.requireNonNull(objectToJson(customerRegistrationRequest))));
+
+        long paymentId = 1L;
+
+        Payment payment = new Payment(
+                paymentId,
+                customerId,
+                new BigDecimal("100.00"),
+                Currency.GBP,
+                "x0x0x0x0",
+                "Zakat"
+        );
+
+        PaymentRequest paymentRequest = new PaymentRequest(payment);
 		
 		// when
-		
+        ResultActions paymentResultActions = mockMvc.perform(post("/api/v1/payment")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Objects.requireNonNull(objectToJson(paymentRequest))));
+
 		// then
 		customerRegResultActions.andExpect(status().isOk());
+        paymentResultActions.andExpect(status().isOk());
+        
+        assertThat(paymentRepository.findById(paymentId))
+        .isPresent()
+        .hasValueSatisfying(p -> assertThat(p).isEqualTo(payment));
+
 
 	}
 	private String objectToJson(Object object) {

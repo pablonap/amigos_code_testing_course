b136eb6a2f065983b7b8a30ce5b8504f7bf07679
chipo <pnapoli33@gmail.com>
2021-03-16 23:54:25 -0300

    Inject CustomerRegistrationService in CustomerRegistrationController to invoke registerNewCustomer() method; Add PaymentController to invoke the endpoint to register a new payment.

--- src/main/java/amigos_code_prj01/customer/CustomerRegistrationController.java
+++ src/main/java/amigos_code_prj01/customer/CustomerRegistrationController.java
@@ -2,6 +2,7 @@ package amigos_code_prj01.customer;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/customer-registration")
 public class CustomerRegistrationController {
 	
+	private final CustomerRegistrationService customerRegistrationService;
+	
+	@Autowired
+	public CustomerRegistrationController(CustomerRegistrationService customerRegistrationService) {
+		this.customerRegistrationService = customerRegistrationService;
+	}
+
 	@PutMapping
 	public void registerNewCustomer(
 			@Valid @RequestBody CustomerRegistrationRequest request) {
-		System.out.println(request);
+		customerRegistrationService.registerNewCustomer(request);
 	}
 
 }
---
+++ src/main/java/amigos_code_prj01/payment/PaymentController.java
@@ -0,0 +1,26 @@
+package amigos_code_prj01.payment;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/payment")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @Autowired
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @RequestMapping("/{customerId}")
+    public void makePayment(@PathVariable("customerId") UUID customerId, @RequestBody PaymentRequest paymentRequest) {
+        paymentService.chargeCard(customerId, paymentRequest);
+    }
+}

288cc0b79ff0e6117f0cf332b28d6066947dfa58
chipo <pnapoli33@gmail.com>
2021-04-02 14:54:05 -0300

    Add more tests with the @DisplayName annotation

--- src/test/java/amigos_code_prj01/testing/utils/PhoneNumberValidatorTest.java
+++ src/test/java/amigos_code_prj01/testing/utils/PhoneNumberValidatorTest.java
@@ -1,9 +1,10 @@
 package amigos_code_prj01.testing.utils;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import amigos_code_prj01.utils.PhoneNumberValidator;
 
@@ -26,6 +27,33 @@ class PhoneNumberValidatorTest {
 		
 		// then
 		assertThat(isValid).isTrue();
+	}
+
+	@Test
+	@DisplayName("Should fail when length is bigger than 13")
+	void itShouldValidatePhoneNumberWhenIncorrectAndHasLengthBiggerThan13() {
+		// given
+		String phoneNumber = "+5411548414441234";
+		
+		// when
+		boolean isValid = underTest.test(phoneNumber);
+		
+		// then
+		assertThat(isValid).isFalse();
+
+	}
+
+	@Test
+	@DisplayName("Should fail when does not start with +")
+	void itShouldValidatePhoneNumberWhenDoesNotStartWithPlusSign() {
+		// given
+		String phoneNumber = "5411548414441234";
+		
+		// when
+		boolean isValid = underTest.test(phoneNumber);
+		
+		// then
+		assertThat(isValid).isFalse();
 
 	}
 

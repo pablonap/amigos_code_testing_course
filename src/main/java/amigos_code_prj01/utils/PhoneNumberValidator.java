fe6d0a9ae004ba777991883ab7f52b55247e4bda
chipo <pnapoli33@gmail.com>
2021-04-02 14:47:41 -0300

    Add the assertion in the test and modify the code implementation

--- src/main/java/amigos_code_prj01/utils/PhoneNumberValidator.java
+++ src/main/java/amigos_code_prj01/utils/PhoneNumberValidator.java
@@ -1,5 +1,11 @@
 package amigos_code_prj01.utils;
 
-public class PhoneNumberValidator {
+import java.util.function.Predicate;
+
+public class PhoneNumberValidator implements Predicate<String> {
+
+	public boolean test(String phoneNumber) {
+		return phoneNumber.startsWith("+5411") && phoneNumber.length() == 13;
+	}
 
 }
--- src/test/java/amigos_code_prj01/testing/utils/PhoneNumberValidatorTest.java
+++ src/test/java/amigos_code_prj01/testing/utils/PhoneNumberValidatorTest.java
@@ -3,6 +3,8 @@ package amigos_code_prj01.testing.utils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import amigos_code_prj01.utils.PhoneNumberValidator;
 
 class PhoneNumberValidatorTest {
@@ -17,10 +19,13 @@ class PhoneNumberValidatorTest {
 	@Test
 	void itShouldValidatePhoneNumber() {
 		// given
+		String phoneNumber = "+541154841444";
 		
 		// when
+		boolean isValid = underTest.test(phoneNumber);
 		
 		// then
+		assertThat(isValid).isTrue();
 
 	}
 

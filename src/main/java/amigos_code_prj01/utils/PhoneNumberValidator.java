58dd6d6abfccb844e06d4d19d59e38c3e8a032f2
chipo <pnapoli33@gmail.com>
2021-04-02 11:57:17 -0300

    Add PhoneNumberValidatorTest class with the minimum code possible. It is, an empty test called "itShouldValidatePhoneNumber" and the class under test "PhoneNumberValidator" which will be instantiated before each test case runs.

---
+++ src/main/java/amigos_code_prj01/utils/PhoneNumberValidator.java
@@ -0,0 +1,5 @@
+package amigos_code_prj01.utils;
+
+public class PhoneNumberValidator {
+
+}
---
+++ src/test/java/amigos_code_prj01/testing/utils/PhoneNumberValidatorTest.java
@@ -0,0 +1,27 @@
+package amigos_code_prj01.testing.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import amigos_code_prj01.utils.PhoneNumberValidator;
+
+class PhoneNumberValidatorTest {
+	
+	private PhoneNumberValidator underTest;
+	
+	@BeforeEach
+	void setUp() {
+		underTest = new PhoneNumberValidator();
+	}
+	
+	@Test
+	void itShouldValidatePhoneNumber() {
+		// given
+		
+		// when
+		
+		// then
+
+	}
+
+}

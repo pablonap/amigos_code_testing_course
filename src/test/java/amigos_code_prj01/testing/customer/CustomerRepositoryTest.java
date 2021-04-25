4842999c4d4e29d343b0b3db816682b8fc9f3641
chipo <pnapoli33@gmail.com>
2021-04-24 22:39:38 -0300

    Add "itShouldReturnFivePageableCustomersInOnePageWhenAskingByAllCustomers" test case in CustomerRepositoryTest class

--- src/test/java/amigos_code_prj01/testing/customer/CustomerRepositoryTest.java
+++ src/test/java/amigos_code_prj01/testing/customer/CustomerRepositoryTest.java
@@ -3,13 +3,19 @@ package amigos_code_prj01.testing.customer;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.TestPropertySource;
 
 import amigos_code_prj01.customer.Customer;
@@ -62,7 +68,7 @@ class CustomerRepositoryTest {
 		
 		// when
 		underTest.save(customer);
-		Optional<Customer> optCustomerFromDb = underTest.findById(1L);
+		Optional<Customer> optCustomerFromDb = underTest.selectCustomerByPhoneNumber(customer.getPhoneNumber());
 		
 		// then
 		assertThat(optCustomerFromDb).isPresent().hasValueSatisfying(c -> {
@@ -109,6 +115,53 @@ class CustomerRepositoryTest {
 		.hasMessageContaining(
 				"not-null property references a null or transient value : amigos_code_prj01.customer.Customer.phoneNumber")
 		.isInstanceOf(DataIntegrityViolationException.class);
+	}
+
+	@Test
+	void itShouldReturnFivePageableCustomersInOnePageWhenAskingByAllCustomers() {
+		// given
+        saveCustomers();
+        
+        PageRequest pageRequest = PageRequest.of(0, 10);
+        
+        // when
+        List<Customer> customersResponse = underTest.findAllPageableCustomers(pageRequest).getContent();
+
+		// then
+        List<Customer> expectedCustomers = LongStream.rangeClosed(1, 5).mapToObj(i -> {
+        	final long id = i;
+        	final String name = "customer_" + i;
+        	final String phoneNumberDigit = String.valueOf(i);
+        	final String phone = phoneNumberDigit + phoneNumberDigit + phoneNumberDigit + phoneNumberDigit;
+        	final String password = "password123";
+
+            Customer customer = new Customer();
+            customer.setId(id);
+            customer.setName(name);
+            customer.setPhoneNumber(phone);
+            customer.setPassword(password);
+            return customer;
+		}).collect(Collectors.toList());
+        
+        int idx = 0;
+        for(Customer expectedCustomer : expectedCustomers) {
+			assertThat(customersResponse.get(idx++).getId()).isEqualTo(expectedCustomer.getId());
+        }
+	}
+
+	private void saveCustomers() {
+		IntStream.rangeClosed(1, 5).mapToObj(i -> {
+        	final String name = "customer_" + i;
+        	final String phoneNumberDigit = String.valueOf(i);
+        	final String phone = phoneNumberDigit + phoneNumberDigit + phoneNumberDigit + phoneNumberDigit;
+        	final String password = "password123";
+
+            Customer customer = new Customer();
+            customer.setName(name);
+            customer.setPhoneNumber(phone);
+            customer.setPassword(password);
+            return customer;
+        }).forEach(c -> underTest.save(c));
 	}
 
 }
